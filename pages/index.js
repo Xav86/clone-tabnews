c2ce47691bd97172f61cfaa67440945996ef742f
Xav86 <gustavo.g.2005@gmail.com>
2024-10-28 11:11:44 -0300

    alterado frase no index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste!</h1>
+    return <h1>olha só que <a href="https://i1.sndcdn.com/artworks-nNv5ZuHaySPVgzc5-5icphQ-t500x500.jpg" target="_blank" rel="external">Imagem Legal!</a></h1>
 }
 
 export default Home;
