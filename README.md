01e00d9036093d67380a9a5f7f02f064278c2330
Xav86 <gustavo.g.2005@gmail.com>
2024-11-11 11:14:01 -0300

    adicionado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto de aprendizado onde vou recriar o https://tabnews.com.br junto ao https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>olha só que 👉<a href="https://i1.sndcdn.com/artworks-nNv5ZuHaySPVgzc5-5icphQ-t500x500.jpg" target="_blank" rel="external">Imagem Legal!</a>👈😎🤙</h1>
+  return (
+    <h1>
+      olha só que 👉
+      <a
+        href="https://i1.sndcdn.com/artworks-nNv5ZuHaySPVgzc5-5icphQ-t500x500.jpg"
+        target="_blank"
+        rel="external"
+      >
+        Imagem Legal!
+      </a>
+      👈😎🤙
+    </h1>
+  );
 }
 
 export default Home;
