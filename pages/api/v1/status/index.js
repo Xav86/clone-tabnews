504873341c845ace584235bd9899f02ec3c1e77c
Xav86 <gustavo.g.2005@gmail.com>
2024-11-25 15:22:30 -0300

    feito desafio do Filipe e funcionou, aqui salvo o registro

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,18 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ msg: "lorem ipsun dolor sit amet" });
+  const updatedAt = new Date().toISOString();
+  const version = await database.query("SHOW server_version");
+  const maxConnections = await database.query("SHOW max_connections");
+  const usedConnections = await database.query(
+    "SELECT COUNT(*) AS conexoes_usadas FROM pg_stat_activity",
+  );
+  res.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: version.rows[0].server_version,
+    max_connections: maxConnections.rows[0].max_connections,
+    used_connections: usedConnections.rows[0].conexoes_usadas,
+  });
 }
 
 export default status;
