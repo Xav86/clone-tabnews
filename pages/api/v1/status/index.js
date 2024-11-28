7f58daa7fee164b76a75663f90fb57120be6d500
Xav86 <gustavo.g.2005@gmail.com>
2024-11-28 08:52:40 -0300

    trabalhando nos dados que retornarão no `/status` mas lidando com um erro estranho

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,16 +2,32 @@ import database from "infra/database.js";
 
 async function status(req, res) {
   const updatedAt = new Date().toISOString();
-  const version = await database.query("SHOW server_version");
-  const maxConnections = await database.query("SHOW max_connections");
-  const usedConnections = await database.query(
-    "SELECT COUNT(*) AS conexoes_usadas FROM pg_stat_activity",
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
   );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  console.log(databaseOpenedConnectionsResult.rows);
+
   res.status(200).json({
     updated_at: updatedAt,
-    postgres_version: version.rows[0].server_version,
-    max_connections: maxConnections.rows[0].max_connections,
-    used_connections: usedConnections.rows[0].conexoes_usadas,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,15 +4,13 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
   console.log(responseBody);
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.postgres_version).toEqual("17.0");
+  expect(responseBody.dependencies.database.version).toEqual("17.0");
 
-  expect(responseBody.max_connections).toBeDefined();
-  expect(responseBody.max_connections).toEqual("100");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
-  expect(responseBody.used_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
